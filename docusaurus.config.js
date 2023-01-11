cc392c6bd3a4f2d06caf503b36b687e761f9b8f2
winwin2011 <yjh2011@live.com>
2023-01-12 00:37:56 +0800

    fix: 更新编辑文章地址

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/YuJianghao/bpmn-js-doc/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/YuJianghao/bpmn-js-doc/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/YuJianghao/bpmn-js-doc/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/YuJianghao/bpmn-js-doc/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
